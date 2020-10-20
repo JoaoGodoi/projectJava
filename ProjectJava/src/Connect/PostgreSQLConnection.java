8f931f628bd78a8d85e1949566c72d48100da46b
João Victor Godoi Bernardino <joaogodoi1010@gmail.com>
2020-10-19 21:39:36 -0300

    conexão meia boca

---
+++ ProjectJava/src/Connect/PostgreSQLConnection.java
@@ -0,0 +1,39 @@
+package Connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgreSQLConnection {
+    
+    public static PostgreSQLConnection instance;
+    
+    private String driver   = "org.postgresql.Driver";
+    private String user     = "postgres";
+    private String password = "123";
+    private String url      = "jdbc:postgresql://localhost:5432/project";
+    private Connection conn = null;
+    
+    public PostgreSQLConnection() {instance = this;}
+    
+    public PostgreSQLConnection(String driver, String user, String password, String url){
+        instance = this;
+        this.driver = driver;
+        this.user = user;
+        this.password = password;
+        this.url = url;
+    }
+    
+    public Connection getConn() { return conn; }
+
+    public static PostgreSQLConnection getInstance() { return PostgreSQLConnection.instance;}
+    
+    public void OpenPSQLConn() throws ClassNotFoundException, SQLException{
+        Class.forName(this.driver);
+        this.conn = DriverManager.getConnection(url, user, password);
+    }
+    
+    public void ClosePSQLConn() throws SQLException{
+        if(this.conn != null) this.conn.close();
+    }
+}
--- ProjectJava/src/projectjava/ProjectJava.java
+++ ProjectJava/src/projectjava/ProjectJava.java
@@ -5,6 +5,12 @@
  */
 package projectjava;
 
+import Connect.PostgreSQLConnection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author joao
@@ -14,8 +20,20 @@ public class ProjectJava {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ClassNotFoundException {
+        PostgreSQLConnection psql = new PostgreSQLConnection();
+        
+        try {
+            PostgreSQLConnection.getInstance().OpenPSQLConn();
+            String query = "INSERT INTO usuario(ysername, senha) VALUES(?, ?);";
+            PreparedStatement stmte = PostgreSQLConnection.getInstance().getConn().prepareStatement(query);
+            stmte.setString(1, "tonho");
+            stmte.setString(2, "top");
+            stmte.execute();
+            PostgreSQLConnection.getInstance().ClosePSQLConn();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProjectJava.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
